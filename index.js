a48bde4f68d94eab01b92926e02bcaf7eb15e62b
Dilara Katuk <dilarakatuk@gmail.com>
2022-06-10 18:09:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,106 @@
 // Your code here
+
+// creating a generic function to create Employee objects
+// const createEmployeeRecord = function (firstName, familyName, title, payPerHour){
+//     this.firstName = firstName
+//     this.familyName = familyName
+//     this.title = title;
+//     this.payPerHour = payPerHour;
+
+// }
+
+// // use "new" keyword to create an object
+// let testEmployee = new createEmployeeRecord("Gray", "Worm", "Security", 1)
+// console.log(testEmployee)
+
+
+const createEmployeeRecord = function (arr) {
+        return {
+            firstName: arr[0],
+            familyName: arr[1],
+            title: arr[2],
+            payPerHour: arr[3],
+            timeInEvents: [],
+            timeOutEvents: [],
+        };
+    };
+
+// let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1])
+// console.log(testEmployee)
+
+const createEmployeeRecords = function (arr) {
+        return arr.map((e) => {
+            return createEmployeeRecord(e);
+        });
+    };
+    
+const createTimeInEvent = function (arr, time) {
+        const timeArr = time.split(" ");
+        const date = timeArr[0];
+        const hour = parseInt(timeArr[1]);
+        arr.timeInEvents.push({
+            type: "TimeIn",
+            date: date,
+            hour: hour,
+        });
+        return arr;
+    };
+    
+const createTimeOutEvent = function (arr, time) {
+        const timeArr = time.split(" ");
+        const date = timeArr[0];
+        const hour = parseInt(timeArr[1]);
+        arr.timeOutEvents.push({
+            type: "TimeOut",
+            date: date,
+            hour: hour,
+        });
+        return arr;
+    };
+    
+const hoursWorkedOnDate = function (arr, date) {
+        let totalHours = 0;
+        arr.timeInEvents.forEach((e) => {
+            if (e.date === date) {
+                totalHours += e.hour;
+            }
+        });
+        arr.timeOutEvents.forEach((e) => {
+            if (e.date === date) {
+                totalHours -= e.hour;
+            }
+        });
+        return Math.abs(totalHours) / 100;
+    };
+    
+const wagesEarnedOnDate = function (arr, date) {
+        let totalWages = 0;
+        arr.timeInEvents.forEach((e) => {
+            if (e.date === date) {
+                totalWages += e.hour * arr.payPerHour;
+            }
+        });
+        arr.timeOutEvents.forEach((e) => {
+            if (e.date === date) {
+                totalWages -= e.hour * arr.payPerHour;
+            }
+        });
+        return Math.abs(totalWages) / 100;
+    };
+    
+const allWagesFor = function (arr) {
+        let totalWages = 0;
+        arr.timeInEvents.map((e) => {
+            totalWages += wagesEarnedOnDate(arr, e.date);
+        });
+        return Math.abs(totalWages);
+    };
+    
+const calculatePayroll = (arr) => {
+        let totalWages = 0;
+        arr.map((e) => {
+            totalWages += allWagesFor(e);
+        });
+        return Math.abs(totalWages);
+    };
+
